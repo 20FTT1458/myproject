585c0951b21b30ce52e790724e5d2f7f069f30b3
geary <20FTT1458@student.pb.edu.bn>
2022-04-15 21:29:04 +0800

    intial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <img alt="My Pic" src="./assets/Geary.jpg" class="h-48 w-48 mx-auto" />
+  <p><b>Nur Aslam Azmi Geary bin Abdul Fattah @ Geary Goodenough</b></p>
+  <p><i>Politeknik Brunei</i></p>
+  <p>Web Development Student</p>
+  <p>20FTT1458</p>
+
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
